1ff0a40dd6125c130c7a40a86aab9abc23f703df
Kenneth <129323640+klavoie1@users.noreply.github.com>
2023-11-05 20:27:06 -0500

    Game settings constants

---
+++ setting.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        # Screen Settings
+        self.SCREEN_WIDTH = 1200
+        self.SCREEN_HEIGHT = 1000
+        self.FPS = 60
+        self.BG_COLOR = (255, 255, 255)
